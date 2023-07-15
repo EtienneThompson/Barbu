fa31b4b2636e192fdf377e65a612b9fc871e1fef
Etienne Thompson <et@etiennethompson.com>
2023-07-15 16:53:14 -0700

    Preload ads for faster viewing time.

--- Assets/Scripts/AdvertisementController.cs
+++ Assets/Scripts/AdvertisementController.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
 public class AdvertisementController : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
 {
-    private string adId;
+    [SerializeField] private string adId;
+    [SerializeField] private bool initialized;
+    [SerializeField] private bool adLoaded;
 
     public void Awake()
     {
@@ -12,6 +15,7 @@ public class AdvertisementController : MonoBehaviour, IUnityAdsInitializationLis
 
     public void InitializeAds()
     {
+        Debug.Log("Initializing ads");
         if (Application.platform == RuntimePlatform.IPhonePlayer)
         {
             this.adId = Constants.AdGameIds.AppleGameId;
@@ -25,15 +29,28 @@ public class AdvertisementController : MonoBehaviour, IUnityAdsInitializationLis
             this.adId = Constants.AdGameIds.AndroidGameId;
         }
 
-        Debug.Log("Using ad id " + this.adId);
         if (!Advertisement.isInitialized && Advertisement.isSupported)
         {
-            Advertisement.Initialize(this.adId);
+            Advertisement.Initialize(this.adId, false, this);
         }
     }
 
     public void RequestToShowInterstitial()
     {
+        // Don't double load ads.
+        if (!this.initialized)
+        {
+            Debug.Log("Ads are not yet initialized");
+            return;
+        }
+
+        if (this.adLoaded)
+        {
+            Debug.Log("Already requested an interstitial");
+            return;
+        }
+
+        Debug.Log("Requesting to show interstitial ad to user");
         if (Application.platform == RuntimePlatform.IPhonePlayer)
         {
             Advertisement.Load(Constants.AdGameIds.AppleInterstitialId, this);
@@ -44,24 +61,61 @@ public class AdvertisementController : MonoBehaviour, IUnityAdsInitializationLis
         }
     }
 
+    public void ShowInterstitialAd()
+    {
+        // Don't show if nothing was loaded.
+        if (!this.initialized)
+        {
+            Debug.Log("Ads are not yet initialized");
+            return;
+        }
+
+        if (!this.adLoaded)
+        {
+            Debug.Log("No interstitial ad loaded");
+            return;
+        }
+
+        string placementId;
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            placementId = Constants.AdGameIds.AppleInterstitialId;
+        }
+        else
+        {
+            placementId = Constants.AdGameIds.AndroidInterstitialId;
+        }
+
+        Debug.Log("Showing ad");
+        Advertisement.Show(placementId, null, this);
+        this.adLoaded = false;
+    }
+
     public void OnInitializationComplete()
     {
+        this.initialized = true;
+        // Because of the asynchronous nature of ads initialization, once we are
+        // initialized, request one because we might have requested an ad via the
+        // round manager before we actually were initialized.
+        this.RequestToShowInterstitial();
         Debug.Log("Unity Ads initialization complete.");
     }
 
     public void OnInitializationFailed(UnityAdsInitializationError error, string message)
     {
+        this.initialized = false;
         Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
     }
 
     public void OnUnityAdsAdLoaded(string placementId)
     {
+        this.adLoaded = true;
         Debug.Log($"Unity Ads Ad Loaded complete - {placementId}");
-        Advertisement.Show(placementId);
     }
 
     public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
     {
+        this.adLoaded = false;
         Debug.Log($"Unity Ads Ad failed to load: {error.ToString()} - {message}");
     }
 
