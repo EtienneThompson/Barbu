d184c69fc2f13456103c94bfc5d7b1116f509bd9
Etienne Thompson <et@etiennethompson.com>
2023-08-28 07:02:57 -0700

    Add settings control for difficulty.

--- Assets/Scripts/UI/Controllers/SettingsMenuController.cs
+++ Assets/Scripts/UI/Controllers/SettingsMenuController.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,32 +6,41 @@ public class SettingsMenuController : MonoBehaviour
 {
     private StateMachine stateMachine;
     private GameObject settingsMenu;
-    private GameObject menuButtons;
     private Button sortingPreviousBtn;
     private Button sortingNextBtn;
+    private Button difficultyPreviousBtn;
+    private Button difficultyNextBtn;
     private Button closeBtn;
     private Label currentlySelectedSortingOption;
+    private Label currentlySelectedDifficulty;
     private int currentSortingOption;
+    private int currentDifficultyOption;
 
     public void OnEnable()
     {
         this.stateMachine = new StateMachine();
         this.stateMachine.SetMenuOpen(true);
-        this.menuButtons = GameObject.Find(Constants.GameObjects.MenuButtons);
         this.settingsMenu = GameObject.Find(Constants.GameObjects.SettingsMenu);
         var document = this.settingsMenu.GetComponent<UIDocument>();
         var root = document.rootVisualElement;
 
         this.sortingPreviousBtn = root.Q<Button>("sortingPrevious");
         this.sortingNextBtn = root.Q<Button>("sortingNext");
+        this.difficultyPreviousBtn = root.Q<Button>("difficultyPrevious");
+        this.difficultyNextBtn = root.Q<Button>("difficultyNext");
         this.closeBtn = root.Q<Button>("close");
 
         this.currentlySelectedSortingOption = root.Q<Label>("currentlySelectedSortingOption");
+        this.currentlySelectedDifficulty = root.Q<Label>("currentlySelectedDifficulty");
         this.currentlySelectedSortingOption.text = Settings.SortingPreference.ToString();
         this.currentSortingOption = Array.IndexOf(Settings.HandSortingOptions, Settings.SortingPreference);
+        this.currentlySelectedDifficulty.text = Settings.ComputerDifficultyPreference.ToString();
+        this.currentDifficultyOption = Array.IndexOf(Settings.ComputerDifficulties, Settings.ComputerDifficultyPreference);
 
         this.sortingPreviousBtn.RegisterCallback<ClickEvent>(HandleSortingPreviousButtonClick);
         this.sortingNextBtn.RegisterCallback<ClickEvent>(HandleSortingNextButtonClick);
+        this.difficultyPreviousBtn.RegisterCallback<ClickEvent>(HandleDifficultyPreviousButtonClick);
+        this.difficultyNextBtn.RegisterCallback<ClickEvent>(HandleDifficultyNextButtonClick);
         this.closeBtn.RegisterCallback<ClickEvent>(HandleCloseButtonClick);
     }
 
@@ -42,6 +49,8 @@ public class SettingsMenuController : MonoBehaviour
         this.stateMachine.SetMenuOpen(false);
         this.sortingPreviousBtn.UnregisterCallback<ClickEvent>(HandleSortingPreviousButtonClick);
         this.sortingNextBtn.UnregisterCallback<ClickEvent>(HandleSortingNextButtonClick);
+        this.difficultyPreviousBtn.UnregisterCallback<ClickEvent>(HandleSortingPreviousButtonClick);
+        this.difficultyNextBtn.UnregisterCallback<ClickEvent>(HandleDifficultyNextButtonClick);
         this.closeBtn.UnregisterCallback<ClickEvent>(HandleCloseButtonClick);
     }
 
@@ -59,6 +68,22 @@ public class SettingsMenuController : MonoBehaviour
         this.currentSortingOption = this.SafeMod(this.currentSortingOption + 1, Settings.HandSortingOptions.Length);
         this.currentlySelectedSortingOption.text = Settings.HandSortingOptions[this.currentSortingOption].ToString();
         Settings.SortingPreference = Settings.HandSortingOptions[this.currentSortingOption];
+    }
+
+    private void HandleDifficultyPreviousButtonClick(ClickEvent evt)
+    {
+        Debug.Log("Difficulty Previous button clicked");
+        this.currentDifficultyOption = this.SafeMod(this.currentDifficultyOption - 1, Settings.ComputerDifficulties.Length);
+        this.currentlySelectedDifficulty.text = Settings.ComputerDifficulties[this.currentDifficultyOption].ToString();
+        Settings.ComputerDifficultyPreference = Settings.ComputerDifficulties[this.currentDifficultyOption];
+    }
+
+    private void HandleDifficultyNextButtonClick(ClickEvent evt)
+    {
+        Debug.Log("Difficulty Next Button clicked");
+        this.currentDifficultyOption = this.SafeMod(this.currentDifficultyOption + 1, Settings.ComputerDifficulties.Length);
+        this.currentlySelectedDifficulty.text = Settings.ComputerDifficulties[this.currentDifficultyOption].ToString();
+        Settings.ComputerDifficultyPreference = Settings.ComputerDifficulties[this.currentDifficultyOption];
     }
 
     private void HandleCloseButtonClick(ClickEvent evt)
