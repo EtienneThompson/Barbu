0374840a93b6d67291db0b8b323c721d22c65811
Etienne Thompson <et@etiennethompson.com>
2024-05-17 17:03:47 -0700

    Update total section.

--- Assets/Scripts/UI/Controllers/ScoreMenuController.cs
+++ Assets/Scripts/UI/Controllers/ScoreMenuController.cs
@@ -10,6 +10,7 @@ namespace Barbu.UI.Controllers
     {
         private Transform ScoreContainer;
         private Transform ScoreRowTemplate;
+        private Transform TotalRow;
 
         private EventsController eventsController;
         private List<Transform> ScoreRows;
@@ -21,6 +22,7 @@ namespace Barbu.UI.Controllers
 
             this.ScoreContainer = transform.Find("ScoreContainer");
             this.ScoreRowTemplate = this.ScoreContainer.Find("ScoreRowTemplate");
+            this.TotalRow = this.ScoreContainer.Find("TotalSection");
 
             // Hide the template object by default.
             this.ScoreRowTemplate.gameObject.SetActive(false);
@@ -48,6 +50,10 @@ namespace Barbu.UI.Controllers
         {
             gameObject.SetActive(true);
 
+            var player1Total = 0;
+            var player2Total = 0;
+            var player3Total = 0;
+            var player4Total = 0;
             for (int i = 0; i < scores[Constants.PlayerIds.Player1].Length; i++)
             {
                 Transform scoreRow = Instantiate(this.ScoreRowTemplate, this.ScoreContainer);
@@ -56,13 +62,25 @@ namespace Barbu.UI.Controllers
                 scoreRow.gameObject.SetActive(true);
                 this.ScoreRows.Add(scoreRow);
 
-                Debug.Log(scoreRow.Find("RoundLabel").GetComponent<TMP_Text>());
                 scoreRow.Find("RoundLabel").GetComponent<TMP_Text>().text = (i + 1).ToString();
-                scoreRow.Find("Player1Label").GetComponent<TMP_Text>().text = scores[Constants.PlayerIds.Player1][i].ToString();
-                scoreRow.Find("Player2Label").GetComponent<TMP_Text>().text = scores[Constants.PlayerIds.Player2][i].ToString();
-                scoreRow.Find("Player3Label").GetComponent<TMP_Text>().text = scores[Constants.PlayerIds.Player3][i].ToString();
-                scoreRow.Find("Player4Label").GetComponent<TMP_Text>().text = scores[Constants.PlayerIds.Player4][i].ToString();
+                var player1Score = scores[Constants.PlayerIds.Player1][i];
+                var player2Score = scores[Constants.PlayerIds.Player2][i];
+                var player3Score = scores[Constants.PlayerIds.Player3][i];
+                var player4Score = scores[Constants.PlayerIds.Player4][i];
+                player1Total += player1Score;
+                player2Total += player2Score;
+                player3Total += player3Score;
+                player4Total += player4Score;
+                scoreRow.Find("Player1Label").GetComponent<TMP_Text>().text = player1Score.ToString();
+                scoreRow.Find("Player2Label").GetComponent<TMP_Text>().text = player2Score.ToString();
+                scoreRow.Find("Player3Label").GetComponent<TMP_Text>().text = player3Score.ToString();
+                scoreRow.Find("Player4Label").GetComponent<TMP_Text>().text = player4Score.ToString();
             }
+
+            this.TotalRow.Find("Player1Label").GetComponent<TMP_Text>().text = player1Total.ToString();
+            this.TotalRow.Find("Player2Label").GetComponent<TMP_Text>().text = player2Total.ToString();
+            this.TotalRow.Find("Player3Label").GetComponent<TMP_Text>().text = player3Total.ToString();
+            this.TotalRow.Find("Player4Label").GetComponent<TMP_Text>().text = player4Total.ToString();
         }
     }
 }
